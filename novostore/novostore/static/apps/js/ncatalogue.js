3469f903487bb42a4a9e031f137e6b8581e71415
Evgeni Makarov <evgeni.makarov@gmail.com>
2013-06-23 03:38:04 +0400

    yell

--- novostore/novostore/static/apps/js/ncatalogue.js
+++ novostore/novostore/static/apps/js/ncatalogue.js
@@ -1,4 +1,5 @@
 var intervals=new Array();
+var previousid = 0;
 $('.dropdown-toggle').click(
   function(){
     window.location.href=$(this).attr('slug');
@@ -8,11 +9,16 @@ $('.dropdown-toggle').hover(
   function(){
     var id = $(this).attr('dropdown_node_id');
     $('#dropdown_node_menu_'+id).show();
+    if (previousid != id){
+      window.clearTimeout(intervals[previousid])
+      $('#dropdown_node_menu_'+previousid).hide();
+      previousid = id;
+    }
     window.clearTimeout(intervals[id])
   },
   function(){
     var id = $(this).attr('dropdown_node_id');
-    var str_fadeout = "$('#dropdown_node_menu_"+id+"').fadeOut()";
+    var str_fadeout = "$('#dropdown_node_menu_"+id+"').hide()";
     intervals[id] = setTimeout(str_fadeout,500);
   }
 );
@@ -23,6 +29,6 @@ $('.dropdown-menu').hover(
     $(this).show();
   },
   function(){
-    $(this).fadeOut();
+    $(this).hide();
   }
 );
