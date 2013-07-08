f1e922510054bbc9f22c17061280e55180528395
Evgeni Makarov <evgeni.makarov@gmail.com>
2013-07-08 14:35:57 +0400

    old links savings

--- novostore/novostore/confs/urls_gsmrepiter.py
+++ novostore/novostore/confs/urls_gsmrepiter.py
@@ -2,5 +2,9 @@ from django.conf.urls.defaults import patterns, include, url
 from novostore.urls import *
 
 urlpatterns = patterns('',
-    (r'^faq/$', 'blog.views.article', {'artid' : 3 }),
+    (r'^sale/$', 'blog.views.article', {'artid' : 5 }),
+    (r'^design/$', 'blog.views.article', {'artid' : 6 }),
+    (r'^installation/$', 'blog.views.article', {'artid' : 7 }),
+    (r'^objects/$', 'blog.views.blogtermpage', {'termslug' : 'objects' }),
+    (r'^faq/$', 'blog.views.blogtermpage', {'termslug' : 'faq' }),
 ) + urlpatterns
