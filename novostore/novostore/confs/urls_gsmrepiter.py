91d79036ae1215939f41d10fc9bfd02efb94d848
Evgeni Makarov <evgeni.makarov@gmail.com>
2013-09-04 01:41:54 +0400

    new page

--- novostore/novostore/confs/urls_gsmrepiter.py
+++ novostore/novostore/confs/urls_gsmrepiter.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     (r'^installation/$', 'blog.views.article', {'artid' : 7 }),
     (r'^team/$', 'blog.views.article', {'artid' : 10 }),
     (r'^contacts/$', 'blog.views.article', {'artid' : 11 }),
-    (r'^objects/$', 'blog.views.blogtermpage', {'termslug' : 'objects' }),
+    (r'^objects/$', 'blog.views.objects'),
+    #(r'^objects/$', 'blog.views.blogtermpage', {'termslug' : 'objects' }),
     (r'^faq/$', 'blog.views.blogtermpage', {'termslug' : 'faq' }),
 ) + urlpatterns
