2ed14103f0e5401de48828d618308ddaee2c32cc
Evgeni Makarov <evgeni.makarov@gmail.com>
2013-09-15 01:53:53 +0400

    lead admin fix

--- novostore/shopleads/admin.py
+++ novostore/shopleads/admin.py
@@ -2,7 +2,7 @@ from models import *
 from django.contrib import admin
 from utils.visibility import VisibleAdmin
 
-class LeadAdmin(VisibleAdmin):
+class LeadAdmin(admin.ModelAdmin):
   list_display = ['shop','received','content']
 
 admin.site.register(Lead,LeadAdmin)
